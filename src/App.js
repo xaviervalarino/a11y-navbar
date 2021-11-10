c83b5d8a449949cf4a7c816c9ced93df8cdacd74
Xavier Valarino <xavier@valarino.com>
2021-11-09 16:01:48 -0800

    Wrap the navbar in a header tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { Box } from 'gestalt';
 import 'gestalt/dist/gestalt.css';
 
 import Navbar from './components/Navbar';
+import BrandingLink from './components/BrandingLink';
 import Annotations from './components/Annotations';
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <Box
+        as='header'
+        role='banner'
+        borderStyle='shadow'
+        width="100vw"
+        padding={2}
+        smPaddingX={6}
+        smPaddingY={4}
+        display='flex'
+        justifyContent="between"
+      >
+        <BrandingLink
+          accessibilityLabel='Developers homepage'
+          role='link'
+          href='/'
+        />
+        <Navbar />
+      </Box>
       <Annotations />
     </>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,6 @@ import {
   TapArea
 } from 'gestalt';
 
-import BrandingLink from './BrandingLink';
 import DocsDropdown from './DocsDropdown';
 
 export default function Navbar() {
@@ -15,61 +14,47 @@ export default function Navbar() {
     <Box
       as='nav'
       aria-label='Site'
-      borderStyle='shadow'
-      width="100vw"
-      padding={2}
-      smPaddingX={6}
-      smPaddingY={4}
+      column={7}
       display='flex'
-      justifyContent="between"
+      justifyContent='end'
     >
-      {/* Passing props down to `TapArea`
-        * Note that the LogoLink is not part of the unordered list of nav links
-        */}
-      <BrandingLink
-        accessibilityLabel='Developers homepage'
-        role='link'
-        href='/'
-      />
-      <Box column={7} display='flex' justifyContent='end'>
-        <Flex role='list' gap={2} alignItems='center'>
-          <Flex.Item role='listitem'>
-            <DocsDropdown />
-          </Flex.Item>
-          <Flex.Item role='listitem'>
-            <Button
-              accessibilityLabel='Apps'
-              text='Apps'
-              color='white'
-              role='link'
-              href='/'
-            />
-          </Flex.Item>
-          <Flex.Item role='listitem'>
-            <TapArea
-              accessibilityLabel='Your Pinterest user page'
-              role='link'
-              rounding='circle'
-              href='/'
-            >
-            <Avatar
-              name='username'
-              accessibilityLabel='user avatar'
-              size='xs'
-            />
-            </TapArea>
-          </Flex.Item>
-          <Flex.Item role='listitem'>
-            <IconButton
-              accessibilityLabel='More options'
-              icon='arrow-down'
-              accessibilityExpanded={false}
-              size='xs'
-              color='darkGray'
-            />
-          </Flex.Item>
-        </Flex>
-      </Box>
+      <Flex role='list' gap={2} alignItems='center'>
+        <Flex.Item role='listitem'>
+          <DocsDropdown />
+        </Flex.Item>
+        <Flex.Item role='listitem'>
+          <Button
+            accessibilityLabel='Apps'
+            text='Apps'
+            color='white'
+            role='link'
+            href='/'
+          />
+        </Flex.Item>
+        <Flex.Item role='listitem'>
+          <TapArea
+            accessibilityLabel='Your Pinterest user page'
+            role='link'
+            rounding='circle'
+            href='/'
+          >
+          <Avatar
+            name='username'
+            accessibilityLabel='user avatar'
+            size='xs'
+          />
+          </TapArea>
+        </Flex.Item>
+        <Flex.Item role='listitem'>
+          <IconButton
+            accessibilityLabel='More options'
+            icon='arrow-down'
+            accessibilityExpanded={false}
+            size='xs'
+            color='darkGray'
+          />
+        </Flex.Item>
+      </Flex>
     </Box>
   );
 }
